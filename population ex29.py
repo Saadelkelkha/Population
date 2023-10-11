239b5114c85c1cd005a89a6032544416a5ba2c11
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-11 09:58:04 +0100

    Add files via upload

---
+++ population ex29.py
@@ -0,0 +1,8 @@
+m=1000000
+a=500000
+i=0
+while a<m :
+    a=a+(a/100)*8
+    m=m+50000
+    i=i+1
+print("the number of years that the population of the city Agadir will exceed that that of the city Marrakech is :",i)
